3858717e654b531554e24a4f64f35d46c7ad3473
Denchan <119429548+Denchan-san@users.noreply.github.com>
2023-09-19 11:11:33 +0300

    fix

---
+++ lab1/CourseraWEB2/CourseraWEB2/Models/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CourseraWEB2.Models
+{
+    public class ApplicationDBContext: DbContext 
+    {
+        private DbSet<Customer> Cistomers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DENCHAN\\DENCHAN;Database=CodeFirstDB;Integrated Security=true;TrustServerCertificate=true;");
+        }
+    }
+}
---
+++ lab1/CourseraWEB2/CourseraWEB3/Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CourseraWEB3.Models;
+
+public partial class Customer
+{
+    public int CustomerId { get; set; }
+
+    public string CutomerName { get; set; } = null!;
+
+    public double? CutomerAmount { get; set; }
+
+    public string? City { get; set; }
+}
