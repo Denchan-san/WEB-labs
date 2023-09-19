1d520634b51662125310d4a93623dd592d223bd6
Denchan <119429548+Denchan-san@users.noreply.github.com>
2023-09-19 11:28:29 +0300

    fix

--- lab1/CourseraWEB2/CourseraWEB2/Models/Customer.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CourseraWEB2.Models
-{
-    public class Customer
-    {
-        public int CustomerId { get; set; }
-        public string? CustomerName { get; set; }
-        public double CustomerAmount { get; set; }    
-    }
-}
--- lab1/CourseraWEB2/CourseraWEB2/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CourseraWEB2.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
